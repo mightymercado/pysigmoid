42ea63269a236e891ee414464cdd8a9a8207ae19
root <mightymercado@gmail.com>
2018-05-17 21:48:43 +0800

    updated readme

--- example/typecast.py
+++ example/typecast.py
@@ -1,5 +1,5 @@
 from PySigmoid import *
-
+from math import *
 set_posit_env(32, 2)
 # float to posit
 print(Posit(3.2))
@@ -14,3 +14,21 @@ print(float(Posit("3.2")))
 print(int(Posit(31238912839)))
 # posit to string
 print(str(Posit("-2.333344")))
+
+a = Posit(1)
+b = Posit(2)
+# Addition
+print(a+b)
+# Subtraction
+print(a-b)
+# Division
+print(a/b)
+# multiplication
+print(a*b)
+# square root
+print(sqrt(a))
+# power
+print(a**b)
+
+# operation with other types works too!
+print(2.5 + a + 3 + b)
